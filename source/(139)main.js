abfa494b510598499d4e77da25a60ed1a7e26089
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-04-10 15:59:11 +0900

    Update (139)main.js

--- source/(139)main.js
+++ source/(139)main.js
@@ -147,46 +147,71 @@ function setWeather(data) {
 }
 
 function displayWeather() {
-	//console.log(weather_dataset);
-	if(weather_dataset == undefined) return;
-	if(weather_dataset.location == undefined) return;
+	if (weather_dataset == undefined) return;
+	if (weather_dataset.location == undefined) return;
 
-	$('#weather > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(2)').html(weather_dataset.location + '<img src="/image/place.png" alt="">');
-	$('#weather > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(1)').html('<img src="/image/weather/' + weather_dataset.icon + '.png" alt="' + weather_dataset.sky + '">');
-	$('#weather > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2)').html(weather_dataset.currentTemperature + '˚');
-	$('#weather > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(2)').html('최저 ' + weather_dataset.minTemperature + '˚');
-	$('#weather > table:nth-child(1) > tbody > tr:nth-child(4) > td:nth-child(1)').html('최고 ' + weather_dataset.maxTemperature + '˚');
-	$('#weather > table:nth-child(1) > tbody > tr:nth-child(4) > td:nth-child(1)').html(weather_dataset.sky);
+	// 지역 + 아이콘
+	$('#weather > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(2)').html(
+		weather_dataset.location + '<img src="/image/place.png" alt="">'
+	);
+
+	// 날씨 아이콘
+	$('#weather > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(1)').html(
+		'<img src="/image/weather/' + weather_dataset.icon + '.png" alt="' + weather_dataset.sky + '">'
+	);
+
+	// 현재 온도
+	$('#weather > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2)').html(
+		weather_dataset.currentTemperature + '˚'
+	);
+	
+
+	// 최고/최저 기온 계산
+	const maxTemp = Math.round(weather_dataset.maxTemperature / 100);
+	const minTemp = Math.round(weather_dataset.minTemperature / 10);
+	
+	// 날씨 상태 (흐림, 맑음 등) → tr 4번째, td 1번째 칸
+	$('#weather > table:nth-child(1) > tbody > tr:nth-child(4) > td:nth-child(1)').html(
+	    weather_dataset.sky
+	);
+	
+	// 최고 + 최저 기온 → tr 4번째, td 2번째 칸
+	$('#weather > table:nth-child(1) > tbody > tr:nth-child(4) > td:nth-child(2)').html(
+	    '최고 ' + maxTemp + '˚ / 최저 ' + minTemp + '˚'
+	);
+
+	
+	// 미세먼지
 	$('#weather > ul > li:nth-child(1) > span').html(weather_dataset.finedust);
-	if(weather_dataset.finedust.indexOf("좋음") >= 0) {
-		$('#weather > ul > li:nth-child(1) > span').addClass("green");
-		$('#weather > ul > li:nth-child(1) > span').removeClass("red");
-	} else if(weather_dataset.finedust.indexOf("나쁨") >= 0) {
-		$('#weather > ul > li:nth-child(1) > span').removeClass("green");
-		$('#weather > ul > li:nth-child(1) > span').addClass("red");
+	if (weather_dataset.finedust.indexOf("좋음") >= 0) {
+		$('#weather > ul > li:nth-child(1) > span').addClass("green").removeClass("red");
+	} else if (weather_dataset.finedust.indexOf("나쁨") >= 0) {
+		$('#weather > ul > li:nth-child(1) > span').addClass("red").removeClass("green");
 	} else {
-		$('#weather > ul > li:nth-child(1) > span').removeClass("green");
-		$('#weather > ul > li:nth-child(1) > span').removeClass("red");
+		$('#weather > ul > li:nth-child(1) > span').removeClass("green red");
 	}
 
+	// 초미세먼지
 	$('#weather > ul > li:nth-child(2) > span').html(weather_dataset.ultrafinedust);
-	if(weather_dataset.ultrafinedust.indexOf("좋음") >= 0) {
-		$('#weather > ul > li:nth-child(2) > span').addClass("green");
-		$('#weather > ul > li:nth-child(2) > span').removeClass("red");
-	} else if(weather_dataset.ultrafinedust.indexOf("나쁨") >= 0) {
-		$('#weather > ul > li:nth-child(2) > span').removeClass("green");
-		$('#weather > ul > li:nth-child(2) > span').addClass("red");
+	if (weather_dataset.ultrafinedust.indexOf("좋음") >= 0) {
+		$('#weather > ul > li:nth-child(2) > span').addClass("green").removeClass("red");
+	} else if (weather_dataset.ultrafinedust.indexOf("나쁨") >= 0) {
+		$('#weather > ul > li:nth-child(2) > span').addClass("red").removeClass("green");
 	} else {
-		$('#weather > ul > li:nth-child(2) > span').removeClass("green");
-		$('#weather > ul > li:nth-child(2) > span').removeClass("red");
+		$('#weather > ul > li:nth-child(2) > span').removeClass("green red");
 	}
 
-	for(var i=0; i<weather_dataset.lists.length; i++) {
-		var hour = weather_dataset.lists[i].date.substring(8,10);
+	// 시간별 예보
+	for (var i = 0; i < weather_dataset.lists.length; i++) {
+		var hour = weather_dataset.lists[i].date.substring(8, 10);
 		var hour_name = hour > 12 ? '오후 ' + (hour - 12) + '시' : '오전 ' + hour + '시';
-		$('#weather > table:nth-child(3) > tbody > tr:nth-child(' + (i+1) + ') > td:nth-child(1)').html(hour_name);
-		$('#weather > table:nth-child(3) > tbody > tr:nth-child(' + (i+1) + ') > td:nth-child(2)').html('<img src="/image/weather/' + weather_dataset.lists[i].icon + '.png" alt="' + weather_dataset.lists[i].sky + '"/>');
-		$('#weather > table:nth-child(3) > tbody > tr:nth-child(' + (i+1) + ') > td:nth-child(3)').html(weather_dataset.lists[i].temperature + '˚');
+		$('#weather > table:nth-child(3) > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(1)').html(hour_name);
+		$('#weather > table:nth-child(3) > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(2)').html(
+			'<img src="/image/weather/' + weather_dataset.lists[i].icon + '.png" alt="' + weather_dataset.lists[i].sky + '"/>'
+		);
+		$('#weather > table:nth-child(3) > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(3)').html(
+			weather_dataset.lists[i].temperature + '˚'
+		);
 	}
 }
 
